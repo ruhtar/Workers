0a31ba4dce7f95890391768f9c7c3d5f7c5ad348
ruhtar <arthur.amorim10@gmail.com>
2024-04-04 20:08:18 -0300

    Project setup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
+using Hangfire;
+using Hangfire.Console;
+using Hangfire.Redis.StackExchange;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(options =>
+{
+    var connectionString = builder.Configuration.GetValue<string>("RedisConnection");
+
+    if (string.IsNullOrEmpty(connectionString))
+        throw new Exception("Empty Connection String from Redis");
+
+    var redis = ConnectionMultiplexer.Connect(connectionString);
+
+    options.UseRedisStorage(redis, options: new RedisStorageOptions() { Prefix = "HANGFIRE" });
+    options.UseConsole();
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseHangfireDashboard("/hangfire");
 
 app.Run();
